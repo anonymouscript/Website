ccdabfad8218dd5d91bc1d661d724c6a69656ffa
anonymouscript <d.organ@outlook.com>
2021-09-26 16:45:18 -0500

    Changed htdocs folder to docs

---
+++ docs/index.js
@@ -0,0 +1,20 @@
+<html>
+	<Head>
+	<tile>redirecting</title>
+		<script type="text/js">
+		window.location.replace(window.location.href + "Home")
+		</script>
+</head>
+	<body>
+This Page should redirect in a few secconds...
+
+
+If it doesn't, check that javascript is enabled
+	</body>
+</html>
+
+
+
+
+
+
